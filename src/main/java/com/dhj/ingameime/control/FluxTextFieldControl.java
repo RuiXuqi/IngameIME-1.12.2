fc260f83717360a79da68fc58bb6a139d7bee548
RuiXuqi <90179819+RuiXuqi@users.noreply.github.com>
2025-11-29 23:38:18 +0800

    Fix flux text field

--- src/main/java/com/dhj/ingameime/control/FluxTextFieldControl.java
+++ src/main/java/com/dhj/ingameime/control/FluxTextFieldControl.java
@@ -2,12 +2,20 @@ package com.dhj.ingameime.control;
 
 import com.dhj.ingameime.ClientProxy;
 import com.dhj.ingameime.mixins.flux.AccessorGuiTextField;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
 import sonar.fluxnetworks.client.gui.basic.GuiTextField;
 
-public class FluxTextFieldControl<T extends GuiTextField> extends AbstractControl<GuiTextField> {
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.awt.*;
 
-    protected FluxTextFieldControl(T control) {
+public class FluxTextFieldControl<T extends GuiTextField> extends AbstractControl<GuiTextField> {
+    private final @Nullable GuiScreen container;
+
+    protected FluxTextFieldControl(T control, @Nullable GuiScreen container) {
         super(control);
+        this.container = container;
     }
 
     @Override
@@ -15,29 +23,16 @@ public class FluxTextFieldControl<T extends GuiTextField> extends AbstractContro
         return this.controlObject.getVisible();
     }
 
-//    private String text;
-//    private int x;
-//    private int y;
-
+    @Nonnull
     @Override
-    public int getCursorX() {
+    public Point getCursorPos() {
         AccessorGuiTextField accessor = (AccessorGuiTextField) this.controlObject;
-        String text = this.controlObject.isTextInvisible ? this.controlObject.getInvisibleText(this.controlObject.getText().length()) : this.controlObject.getText();
-//        if (!Objects.equals(this.text, text) || this.x != this.controlObject.x || this.y != this.controlObject.y) {
-//            IngameIME_Forge.LOG.info("text: {}, x: {}, y: {}", text, this.controlObject.x, this.controlObject.y);
-//        }
-//        this.text = text;
-//        this.x = this.controlObject.x;
-//        this.y = this.controlObject.y;
-        return AbstractControl.getCursorX(accessor.getFont(), text,
-                this.controlObject.x + 4, this.controlObject.getWidth(),
+        return AbstractControl.getContainerCursorPos(
+                this.container, accessor.getFont(), this.controlObject.getText(),
+                this.controlObject.x, this.controlObject.y, this.controlObject.width, this.controlObject.height,
                 accessor.getLineScrollOffset(), this.controlObject.getCursorPosition(), this.controlObject.getSelectionEnd(),
-                this.controlObject.getEnableBackgroundDrawing());
-    }
-
-    @Override
-    public int getCursorY() {
-        return AbstractControl.getCursorY(this.controlObject.y + (this.controlObject.height - 8) / 2, this.controlObject.height, this.controlObject.getEnableBackgroundDrawing());
+                true
+        );
     }
 
     /**
@@ -46,6 +41,6 @@ public class FluxTextFieldControl<T extends GuiTextField> extends AbstractContro
      * @param object The field to be set
      */
     public static void onFocusChange(GuiTextField object, boolean focused) {
-        ClientProxy.INSTANCE.onControlFocus(new FluxTextFieldControl<>(object), focused, false);
+        ClientProxy.INSTANCE.onControlFocus(new FluxTextFieldControl<>(object, Minecraft.getMinecraft().currentScreen), focused, false);
     }
 }
